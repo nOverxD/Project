1bfa513654fb96cade0d1c689cc63c79a4101c4b
unitifree <christophe.michel@unitifree.be>
2015-08-16 03:59:27 +0200

    Add Function Random in manageFactory

--- ChristmasVillage/frmFactoryManage.Designer.cs
+++ ChristmasVillage/frmFactoryManage.Designer.cs
@@ -28,52 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.lblNameofToys = new System.Windows.Forms.Label();
+            this.btnProductionToys = new System.Windows.Forms.Button();
+            this.btnReturn = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tbxPriceToys = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tbxNbrProduction = new System.Windows.Forms.TextBox();
+            this.cbxTimeProduction = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // lblNameofToys
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(175, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Name :: [Given by FactoryManager]";
+            this.lblNameofToys.AutoSize = true;
+            this.lblNameofToys.Location = new System.Drawing.Point(13, 13);
+            this.lblNameofToys.Name = "lblNameofToys";
+            this.lblNameofToys.Size = new System.Drawing.Size(47, 13);
+            this.lblNameofToys.TabIndex = 0;
+            this.lblNameofToys.Text = "Name :: ";
             // 
-            // button1
+            // btnProductionToys
             // 
-            this.button1.Location = new System.Drawing.Point(37, 226);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(92, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Production Toys";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnProductionToys.Location = new System.Drawing.Point(37, 226);
+            this.btnProductionToys.Name = "btnProductionToys";
+            this.btnProductionToys.Size = new System.Drawing.Size(92, 23);
+            this.btnProductionToys.TabIndex = 1;
+            this.btnProductionToys.Text = "Production Toys";
+            this.btnProductionToys.UseVisualStyleBackColor = true;
+            this.btnProductionToys.Click += new System.EventHandler(this.btnProductionToys_Click);
             // 
-            // button2
+            // btnReturn
             // 
-            this.button2.Location = new System.Drawing.Point(165, 226);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Cancel";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnReturn.Location = new System.Drawing.Point(165, 226);
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(75, 23);
+            this.btnReturn.TabIndex = 2;
+            this.btnReturn.Text = "Return";
+            this.btnReturn.UseVisualStyleBackColor = true;
+            this.btnReturn.Click += new System.EventHandler(this.btnReturn_Click);
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.tbxPriceToys);
             this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.tbxNbrProduction);
+            this.groupBox1.Controls.Add(this.cbxTimeProduction);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Location = new System.Drawing.Point(16, 42);
@@ -83,12 +85,12 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Toys of Production";
             // 
-            // textBox2
+            // tbxPriceToys
             // 
-            this.textBox2.Location = new System.Drawing.Point(120, 89);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(121, 20);
-            this.textBox2.TabIndex = 5;
+            this.tbxPriceToys.Location = new System.Drawing.Point(120, 89);
+            this.tbxPriceToys.Name = "tbxPriceToys";
+            this.tbxPriceToys.Size = new System.Drawing.Size(121, 20);
+            this.tbxPriceToys.TabIndex = 5;
             // 
             // label4
             // 
@@ -99,26 +101,27 @@
             this.label4.TabIndex = 4;
             this.label4.Text = "Nbr Production";
             // 
-            // textBox1
+            // tbxNbrProduction
             // 
-            this.textBox1.Enabled = false;
-            this.textBox1.Location = new System.Drawing.Point(120, 53);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(121, 20);
-            this.textBox1.TabIndex = 3;
+            this.tbxNbrProduction.Enabled = false;
+            this.tbxNbrProduction.Location = new System.Drawing.Point(120, 53);
+            this.tbxNbrProduction.Name = "tbxNbrProduction";
+            this.tbxNbrProduction.Size = new System.Drawing.Size(121, 20);
+            this.tbxNbrProduction.TabIndex = 3;
             // 
-            // comboBox1
+            // cbxTimeProduction
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.cbxTimeProduction.FormattingEnabled = true;
+            this.cbxTimeProduction.Items.AddRange(new object[] {
             "1",
             "2",
             "4",
             "8"});
-            this.comboBox1.Location = new System.Drawing.Point(120, 17);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 2;
+            this.cbxTimeProduction.Location = new System.Drawing.Point(120, 17);
+            this.cbxTimeProduction.Name = "cbxTimeProduction";
+            this.cbxTimeProduction.Size = new System.Drawing.Size(121, 21);
+            this.cbxTimeProduction.TabIndex = 2;
+            this.cbxTimeProduction.SelectedValueChanged += new System.EventHandler(this.cbxTimeProduction_SelectedValueChanged);
             // 
             // label3
             // 
@@ -144,11 +147,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReturn);
+            this.Controls.Add(this.btnProductionToys);
+            this.Controls.Add(this.lblNameofToys);
             this.Name = "frmFactoryManage";
             this.Text = "frmFactorySaleToys";
+            this.Load += new System.EventHandler(this.frmFactoryManage_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -158,14 +162,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblNameofToys;
+        private System.Windows.Forms.Button btnProductionToys;
+        private System.Windows.Forms.Button btnReturn;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox tbxPriceToys;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox tbxNbrProduction;
+        private System.Windows.Forms.ComboBox cbxTimeProduction;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
     }
--- ChristmasVillage/frmFactoryManage.cs
+++ ChristmasVillage/frmFactoryManage.cs
@@ -22,5 +22,53 @@ namespace ChristmasVillage
             this.user = user;
             this.factory = factory;
         }
+        /// <summary>
+        /// Function to choose a number between min and max 
+        /// </summary>
+        /// <param name="min">50</param>
+        /// <param name="max">100</param>
+        /// <param name="includeNegatives"></param>
+        /// <returns></returns>
+        public static int RandomRange(int min, int max, bool includeNegatives = false)
+        {
+            if (min >= max) throw new Exception("min can't be greater than max");
+            Random rdm = new Random();
+            int num = 0;
+            while (num < min) num = rdm.Next(max + 1);
+            return num * (rdm.Next() % 2 == 0 ? -1 : 1);
+        }
+        private void frmFactoryManage_Load(object sender, EventArgs e)
+        {
+            //lblNameofToys.txt = Receives the name of Toys 
+            
+        }
+        private void cbxTimeProduction_SelectedValueChanged(object sender, EventArgs e)
+        {
+            tbxPriceToys.Text = "75";
+            tbxNbrProduction.Text = (RandomRange(50, 100)).ToString();
+        }
+        /// <summary>
+        /// Function button start production of Toys
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnProductionToys_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// Function button pour n'appliquer aucun update
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnReturn_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        
+
+       
+
     }
 }

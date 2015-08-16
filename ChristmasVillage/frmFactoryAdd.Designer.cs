b669c1b7e8be21ec21a41d56ada622bcf67537af
ChristmasVillage <Christmas1>
2015-08-16 10:08:21 +0200

    Affichage

--- ChristmasVillage/frmFactoryAdd.Designer.cs
+++ ChristmasVillage/frmFactoryAdd.Designer.cs
@@ -33,37 +33,41 @@
             this.btnCancel = new System.Windows.Forms.Button();
             this.btnAdd = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.lblFactoryPrice = new System.Windows.Forms.Label();
             this.lblPriceProduction = new System.Windows.Forms.Label();
             this.lblSalePrice = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // cbxFactoryType
             // 
             this.cbxFactoryType.FormattingEnabled = true;
-            this.cbxFactoryType.Location = new System.Drawing.Point(15, 119);
+            this.cbxFactoryType.Location = new System.Drawing.Point(7, 151);
+            this.cbxFactoryType.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.cbxFactoryType.Name = "cbxFactoryType";
-            this.cbxFactoryType.Size = new System.Drawing.Size(257, 21);
+            this.cbxFactoryType.Size = new System.Drawing.Size(384, 28);
             this.cbxFactoryType.TabIndex = 0;
             this.cbxFactoryType.SelectedIndexChanged += new System.EventHandler(this.cbxFactoryType_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 103);
+            this.label1.Location = new System.Drawing.Point(7, 117);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.Size = new System.Drawing.Size(154, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "Choose your factory:";
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(15, 146);
+            this.btnCancel.Location = new System.Drawing.Point(12, 214);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.Size = new System.Drawing.Size(112, 35);
             this.btnCancel.TabIndex = 2;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -71,9 +75,10 @@
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(197, 146);
+            this.btnAdd.Location = new System.Drawing.Point(304, 214);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.Size = new System.Drawing.Size(112, 35);
             this.btnAdd.TabIndex = 3;
             this.btnAdd.Text = "Confirm";
             this.btnAdd.UseVisualStyleBackColor = true;
@@ -82,84 +87,92 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 31);
+            this.label2.Location = new System.Drawing.Point(7, 22);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.Size = new System.Drawing.Size(105, 20);
             this.label2.TabIndex = 4;
             this.label2.Text = "Factory Price:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(126, 9);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(25, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Info";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 54);
+            this.label4.Location = new System.Drawing.Point(7, 54);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.Size = new System.Drawing.Size(128, 20);
             this.label4.TabIndex = 6;
             this.label4.Text = "Price Production:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 80);
+            this.label5.Location = new System.Drawing.Point(7, 86);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.Size = new System.Drawing.Size(84, 20);
             this.label5.TabIndex = 7;
             this.label5.Text = "Sale Price:";
             // 
             // lblFactoryPrice
             // 
             this.lblFactoryPrice.AutoSize = true;
-            this.lblFactoryPrice.Location = new System.Drawing.Point(90, 31);
+            this.lblFactoryPrice.Location = new System.Drawing.Point(124, 22);
+            this.lblFactoryPrice.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblFactoryPrice.Name = "lblFactoryPrice";
-            this.lblFactoryPrice.Size = new System.Drawing.Size(0, 13);
+            this.lblFactoryPrice.Size = new System.Drawing.Size(0, 20);
             this.lblFactoryPrice.TabIndex = 8;
             // 
             // lblPriceProduction
             // 
             this.lblPriceProduction.AutoSize = true;
-            this.lblPriceProduction.Location = new System.Drawing.Point(106, 54);
+            this.lblPriceProduction.Location = new System.Drawing.Point(148, 54);
+            this.lblPriceProduction.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPriceProduction.Name = "lblPriceProduction";
-            this.lblPriceProduction.Size = new System.Drawing.Size(0, 13);
+            this.lblPriceProduction.Size = new System.Drawing.Size(0, 20);
             this.lblPriceProduction.TabIndex = 9;
             // 
             // lblSalePrice
             // 
             this.lblSalePrice.AutoSize = true;
-            this.lblSalePrice.Location = new System.Drawing.Point(76, 80);
+            this.lblSalePrice.Location = new System.Drawing.Point(103, 86);
+            this.lblSalePrice.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblSalePrice.Name = "lblSalePrice";
-            this.lblSalePrice.Size = new System.Drawing.Size(0, 13);
+            this.lblSalePrice.Size = new System.Drawing.Size(0, 20);
             this.lblSalePrice.TabIndex = 10;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lblSalePrice);
+            this.groupBox1.Controls.Add(this.lblFactoryPrice);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cbxFactoryType);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.lblPriceProduction);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(404, 194);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Info";
             // 
             // frmFactoryAdd
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 180);
-            this.Controls.Add(this.lblSalePrice);
-            this.Controls.Add(this.lblPriceProduction);
-            this.Controls.Add(this.lblFactoryPrice);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(428, 261);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.cbxFactoryType);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmFactoryAdd";
             this.Text = "frmFactoryAdd";
             this.Load += new System.EventHandler(this.frmFactoryAdd_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -170,12 +183,12 @@
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label lblFactoryPrice;
         private System.Windows.Forms.Label lblPriceProduction;
         private System.Windows.Forms.Label lblSalePrice;
+        private System.Windows.Forms.GroupBox groupBox1;
 
     }
 }
